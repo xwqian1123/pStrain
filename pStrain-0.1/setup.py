0283b9216916800016b71feeb2d797f56f37e3c7
xwqian1123 <118893441+xwqian1123@users.noreply.github.com>
2022-12-05 10:01:24 +0800

    Add files via upload

---
+++ pStrain-0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+# encoding: utf-8
+
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+
+name="pStrain",
+
+version="0.1",
+
+license="MIT Licence",
+
+
+url="https://github.com/xwqian1123/pStrain",
+
+author="xxx",
+
+author_email="",
+
+
+packages=find_packages(),
+
+include_package_data=True,
+
+platforms="any",
+
+install_requires=[]
+
+)
